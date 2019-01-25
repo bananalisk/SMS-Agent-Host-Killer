543a3cec01b97e68168d663ac5f4c44fce806522
Thomas Lundberg <thomas.lundberg@gmail.com>
2019-01-25 14:08:06 +0100

    v1

---
+++ SMSKiller/ProjectInstaller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace SMSKiller
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+            serviceInstaller1.StartType = ServiceStartMode.Automatic;
+            serviceInstaller1.AfterInstall += OnServiceInstaller1OnAfterInstall;
+        }
+
+        private void OnServiceInstaller1OnAfterInstall(object sender, InstallEventArgs e)
+        {
+            using (ServiceController sc = new ServiceController(serviceInstaller1.ServiceName))
+            {
+                sc.Start();
+            }
+        }
+    }
+}
---
+++ SMSKiller/Service1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace SMSKiller
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer.Elapsed += OnTimerOnElapsed;
+            timer.Interval = 15000;
+            timer.Enabled = true;
+        }
+
+        private void OnTimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            if (IsWithinOfficeHours())
+            {
+                var smsAgentHostService = new ServiceController("CcmExec");
+                if (smsAgentHostService.Status != ServiceControllerStatus.Stopped)
+                {
+                    smsAgentHostService.Stop();
+                }
+            }
+        }
+
+        private static bool IsWithinOfficeHours()
+        {
+            var now = DateTime.Now;
+
+            var isWeekDay = now.DayOfWeek == DayOfWeek.Monday
+                            || now.DayOfWeek == DayOfWeek.Tuesday
+                            || now.DayOfWeek == DayOfWeek.Wednesday
+                            || now.DayOfWeek == DayOfWeek.Thursday
+                            || now.DayOfWeek == DayOfWeek.Friday;
+
+            var isWorkHours = now.Hour > 8 && now.Hour < 19;
+
+            return isWeekDay && isWorkHours;
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
